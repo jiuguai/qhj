7edc363482a1311c150de37c691dfb3d06312ead
jiuguai <jijiyezero@163.com>
2020-01-28 18:31:14 +0800

    add PDFTool.py modify run.py

--- ZERO/run.py
+++ ZERO/run.py
@@ -3,42 +3,76 @@ import sys
 import importlib
 
 select_map = {
+	'describe':'[2] 运单信息',
+	'func':None,
 	1:{
-		"func":"app.order_sys2local_process.run",
-		"describe":"[1] 提取订单信息"
+		'describe':'[1] 订单信息',
+		'func':None,
+		1:{
+			"func":"app.order_sys2local_process.run",
+			"describe":"[1] 商城订单信息提取"
+		},
+		2:{
+			"func":"app.order_insert_mysql.run",
+			"describe":"[2] 同步本地"
+		},
 	},
 	2:{
-		"func":"app.order_insert_mysql.run",
-		"describe":"[2] 将订单信息备份到本地数据"
+		'describe':'[2] 运单信息',
+		'func':None,
+		1:{
+			"func":"app.waybill_update_mall.run",
+			"describe":"[1] 商城运单信息更新"
+		},
+		2:{
+			"func":"app.waybill_update_mysql.run",
+			"describe":"[2] 同步本地"
+		},
 	},
+	
 	3:{
-		"func":"app.waybill_update_mall.run",
-		"describe":"[3] 更新运单信息到系统"
-	},
-	4:{
-		"func":"app.waybill_update_mysql.run",
-		"describe":"[4] 更新运单信息到本地"
-	},
-	5:{
 		"func":"app.order_trace.run",
-		"describe":"[5] 生成订单跟踪信息"
+		"describe":"[3] 生成订单跟踪信息"
 	}
 }
 
+l = [select_map]
+
 while True:
-	for i in range(1, len(select_map)+1):
-		print(select_map[i]['describe'])
-	print('[0] 退出')
+
+	handle_map = l[-1]
+
+	for i in range(1, len(handle_map)-1):
+		print(handle_map[i]['describe'])
+
+	if len(l) == 0:
+		print('[q] 退出')
+	else:
+		print('[0] 返回上级')
+		print('[q] 退出')
+
 	a = input('请输入>>>>')
 	if a.isnumeric():
 		a = int(a)
+
 		if a == 0:
-			break
-		imp_name = select_map[a]['func']
+			if len(l) > 1:
+				l.pop()
+			continue
+		
+		imp_name = handle_map[a]['func']
+
+		if imp_name is None:
+			l.append(handle_map[a])
+			continue
+
 		module_name, func_name = imp_name.rsplit('.',1)
 
 		module = importlib.import_module(module_name)
 		func = getattr(module, func_name)
 		func()
+
+	elif a == 'q':
+		break
 
 
--- ZERO/tools/tool/normal.py
+++ ZERO/tools/tool/normal.py
@@ -4,7 +4,8 @@ __all__ = ["clear_folder", "get_new_file_path",
              "completion_col","get_duplicated_field",
              "start_time", "end_time",
              "Macro","MyEncoder",
-             "read_xl"
+             "read_xl",
+             "PDFTool"
              ]
 import re
 import os
@@ -298,3 +299,95 @@ def read_xl(file_name, *args, deep_search_row=5, index=False,**kargs):
             if compare_fields <= set(sht.row_values(row)):
                 data = file_obj.parse(sheet_name=sht_name,header=row,index=index,**kargs)
                 return data
+
+
+
+
+# PDF 工具
+class PDFTool():
+
+    @staticmethod
+    def pdf_split(file_path, result_dir, suffix=""):
+        from PyPDF2 import PdfFileReader, PdfFileWriter
+        with open(file_path, 'rb') as f:
+            reader = PdfFileReader(f)
+
+            nums = reader.getNumPages()
+            for page in range(nums):
+                writer = PdfFileWriter() 
+                writer.addPage(reader.getPage(page))
+                out_file = os.path.join(result_dir, "%s%s.pdf" %(str(page+1).zfill(3), suffix))
+
+                print('拆分 第%s页 ' %(page+1))
+                with open(out_file, 'wb') as outf:
+                    writer.write(outf)
+
+
+    @staticmethod
+    def pdf_merge(file_dir, out_file_path):
+        from PyPDF2 import PdfFileReader, PdfFileWriter
+        writer = PdfFileWriter() 
+        files = [os.path.join(file_dir, file_name) for file_name in os.listdir(file_dir) 
+                    if file_name.endswith('pdf')
+        ]
+        file_objs = [open(file_path, 'rb') for file_path in files
+                    if os.path.isfile(file_path)]
+
+        # print(file_objs)
+
+        for obj in file_objs:
+            reader = PdfFileReader(obj)
+            nums = reader.getNumPages()
+            print(obj)
+            for page in range(nums):
+                print(page)
+                writer.addPage(reader.getPage(page))
+
+        with open(out_file_path, 'wb') as outf:
+            writer.write(outf)
+
+        for obj in file_objs:
+            obj.close()
+
+
+    @staticmethod
+    def extract_table(pdf_path, pages='all', mode="L"):
+        """
+        Arguments:
+            pdf_path {str} -- path
+        
+        Keyword Arguments:
+            pages {str} --  (default: {'all'})
+                  {list} -- [1, 2, 3]
+            mode {str} -- L return List
+                       -- D return DataFrame List
+
+        """
+        mode = mode.upper()
+        if mode == "L":
+            import pdfplumber
+
+            with pdfplumber.open(pdf_path) as pdf:
+                pdf_pages = pdf.pages
+                page_count = len(pdf_pages)
+
+                print("共%s 页" %page_count)
+                if pages == "all":
+                    pages = range(page_count)
+                else:
+                    pages = [page-1 for page in pages]
+
+                data = []
+
+                for page in pages:
+                    for row in pdf_pages[page].extract_tables():
+                        data.extend(row)
+
+                return data
+
+        elif mode == "D":
+            import tabula
+            tables = tabula.read_pdf(pdf_path, pages=pages) 
+            print("共%s 页" %len(tables))
+
+            return tables
